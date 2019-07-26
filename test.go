d308d1f64fe218c9de5f17ed867179987f8a853f
felikesw <felikes.will@gmail.com>
2019-07-26 02:27:51 +0100

    ignore json

--- test.go
+++ test.go
@@ -1,70 +1,46 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"os"
-	"time"
-
-	"github.com/tarm/serial"
+	"net/http"
 )
 
 func main() {
-	port0 := os.Getenv("COM3")
-	port1 := os.Getenv("COM3")
-
-	c0 := &serial.Config{Name: port0, Baud: 115200}
-	c1 := &serial.Config{Name: port1, Baud: 115200}
-
-	s1, err := serial.OpenPort(c0)
-	if err != nil {
-		log.Println(s1, c0)
-
-		log.Fatal(err)
-
-	}
-
-	s2, err := serial.OpenPort(c1)
+	server, err := socketio.NewServer(nil)
 	if err != nil {
 		log.Fatal(err)
 	}
+	server.OnConnect("/", func(s socketio.Conn) error {
+		s.SetContext("")
+		fmt.Println("connected:", s.ID())
+		return nil
+	})
+	server.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
+		fmt.Println("notice:", msg)
+		s.Emit("reply", "have "+msg)
+	})
+	server.OnEvent("/chat", "msg", func(s socketio.Conn, msg string) string {
+		s.SetContext(msg)
+		return "recv " + msg
+	})
+	server.OnEvent("/", "bye", func(s socketio.Conn) string {
+		last := s.Context().(string)
+		s.Emit("bye", last)
+		s.Close()
+		return last
+	})
+	server.OnError("/", func(e error) {
+		fmt.Println("meet error:", e)
+	})
+	server.OnDisconnect("/", func(s socketio.Conn, msg string) {
+		fmt.Println("closed", msg)
+	})
+	go server.Serve()
+	defer server.Close()
 
-	ch := make(chan int, 1)
-	go func() {
-		buf := make([]byte, 128)
-		var readCount int
-		for {
-			n, err := s2.Read(buf)
-			if err != nil {
-				log.Fatal(err)
-			}
-			readCount++
-			log.Printf("Read %v %v bytes: % 02x %s", readCount, n, buf[:n], buf[:n])
-			select {
-			case <-ch:
-				ch <- readCount
-				close(ch)
-			default:
-			}
-		}
-	}()
-
-	if _, err = s1.Write([]byte("hello")); err != nil {
-		log.Fatal(err)
-	}
-	if _, err = s1.Write([]byte(" ")); err != nil {
-		log.Fatal(err)
-	}
-	time.Sleep(time.Second)
-	if _, err = s1.Write([]byte("world")); err != nil {
-		log.Fatal(err)
-	}
-	time.Sleep(time.Second / 10)
-
-	ch <- 0
-	s1.Write([]byte(" ")) // We could be blocked in the read without this
-	c := <-ch
-	exp := 5
-	if c >= exp {
-		log.Fatalf("Expected less than %v read, got %v", exp, c)
-	}
+	http.Handle("/socket.io/", server)
+	http.Handle("/", http.FileServer(http.Dir("./asset")))
+	log.Println("Serving at localhost:8000...")
+	log.Fatal(http.ListenAndServe(":8000", nil))
 }
